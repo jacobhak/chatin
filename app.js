03f1c77617b7cf94681e2e417c5b2735fe5f5329
Jacob Håkansson <jacobhakansson@gmail.com>
2013-05-03 01:28:35 +0200

    Added css file. Map showing correctly.

--- app.js
+++ app.js
@@ -4,14 +4,16 @@
 var map;
 
 function initialize () {
-  google.maps.event.addDomListener(window, 'ready', setupMap);
+  google.maps.event.addDomListener(window, "load", setupMap);
+  console.log("jodu");
 }
 
 function setupMap () {
   var mapOptions = {
     center: new google.maps.LatLng(59.347283, 18.073668),
     zoom: 18,
-    disableDefaultUI: true
+    disableDefaultUI: true,
+    mapTypeId: google.maps.MapTypeId.HYBRID
   };
   map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
 }
