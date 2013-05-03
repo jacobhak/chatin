c0333a2df26bf0b9f261e2e3e3004a68e04a36f7
Jacob Håkansson <jacobhakansson@gmail.com>
2013-05-03 02:17:28 +0200

    Added geolocation

--- app.js
+++ app.js
@@ -9,8 +9,17 @@ function initialize () {
 }
 
 function setupMap () {
+  var pos;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      pos = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
+      if (map) map.setCenter(pos);
+    });
+  }
+  pos = new google.maps.LatLng(59.347283,18.073668);
+
   var mapOptions = {
-    center: new google.maps.LatLng(59.347283, 18.073668),
+    center: pos,
     zoom: 18,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   };
